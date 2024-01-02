02a305c622f0aaa453e3facd583353e26f4567f9
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-02 20:27:28 +0530

    Section 6 of basics started

---
+++ Basics_Section_6.sql
@@ -0,0 +1,20 @@
+USE BikeStores;
+
+----------------------------------------------------------------------------------------
+-- GROUPING DATA
+
+--1. GROUP BY
+-- The GROUP BY clause allows you to arrange the rows of a query in groups. 
+-- The groups are determined by the columns that you specify in the GROUP BY clause.
+-- The GROUP BY clause is often used with aggregate functions for generating summary reports.
+-- An aggregate function performs a calculation on a group and returns a unique value per group.
+-- The GROUP BY clause arranges rows into groups and an aggregate function returns the summary for each group.
+
+--2. HAVING
+
+--3. GROUPING SETS
+
+--4. CUBE
+
+--5. ROLLUP
+----------------------------------------------------------------------------------------
