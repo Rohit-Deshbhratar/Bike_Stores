8722ca46959341e6eb14bd857ff444cfc852c8ca
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2024-01-06 11:59:41 +0530

    GROUP BY function in Gasics_Section_6 completed

--- Basics_Section_6.sql
+++ Basics_Section_6.sql
@@ -18,3 +18,112 @@ USE BikeStores;
 
 --5. ROLLUP
 ----------------------------------------------------------------------------------------
+--GROUP BY
+--1. SIMPLE EXAMPLE
+-- Retrieve the customer id and the ordered year of the customers with customer id one and two
+SELECT
+	customer_id, YEAR(order_date) ORDER_YEAR
+FROM
+	[sales].[orders]
+WHERE
+	customer_id IN (1, 2)
+GROUP BY
+	customer_id, YEAR(order_date)
+ORDER BY
+	customer_id;
+
+-- OR
+SELECT DISTINCT
+	customer_id, YEAR(order_date) ORDER_YEAR
+FROM
+	[sales].[orders]
+WHERE
+	customer_id IN (1, 2)
+ORDER BY
+	customer_id;
+
+--2 GROUP BY WITH AGGREGATE FUNCTIONS
+-- Find the number of orders placed by the customer by year
+SELECT
+	customer_id, YEAR(order_date)ORDER_YEAR, COUNT(order_id) ORDER_PLACED
+FROM	
+	[sales].[orders]
+WHERE
+	customer_id IN (1, 2)
+GROUP BY
+	customer_id, YEAR(order_date)
+ORDER BY
+	YEAR(order_date) DESC
+
+--3. Find the number of customers in every city
+SELECT
+	city, COUNT(customer_id) TOTAL_CUSTOMERS
+FROM	
+	[sales].[customers]
+GROUP BY
+	city
+ORDER BY
+	city;
+
+--4. Find the number of customers by state and city
+SELECT
+	state, city, COUNT(customer_id) TOTAL_CUSTOMERS
+FROM
+	[sales].[customers]
+GROUP BY
+	state, city
+ORDER BY
+	state, city;
+
+--5. Total customers by state
+SELECT
+	state, COUNT(customer_id) TOTAL_CUSTOMERS
+FROM
+	[sales].[customers]
+GROUP BY
+	state
+ORDER BY
+	state;
+
+--6. Find the average list price by brand for all products with the model year 2018
+SELECT
+	B.brand_name, 
+	CAST(ROUND(AVG(P.list_price), 2) AS decimal (10, 2)) AVG_PRICE
+FROM
+	[production].[brands] B
+INNER JOIN
+	[production].[products] P
+ON
+	B.brand_id = P.brand_id
+WHERE
+	P.model_year = 2018
+GROUP BY
+	B.brand_name
+ORDER BY
+	B.brand_name;
+
+--7. Find the minimum and maximum list prices of all products with the model 2018 by brand
+SELECT
+	B.brand_name, MIN(P.list_price)MIN_PRICE, MAX(P.list_price)MAX_PRICE
+FROM
+	[production].[products] P
+INNER JOIN
+	[production].[brands] B
+ON
+	P.brand_id = B.brand_id
+WHERE
+	P.model_year = 2018
+GROUP BY
+	B.brand_name
+ORDER BY
+	B.brand_name;
+
+--8. GROUP BY WITH SUM() FUNCTION
+-- Find the net value of every order
+SELECT
+	order_id,
+	SUM(quantity * list_price * (1 - discount))NET_VALUE
+FROM
+	[sales].[order_items]
+GROUP BY
+	order_id;
